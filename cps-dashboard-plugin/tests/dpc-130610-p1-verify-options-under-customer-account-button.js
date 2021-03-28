2c1fed74cfd9b959b8bda5181899ce41294ae52e
firods <shrikantfirodiya4@gmail.com>
2021-03-28 12:31:28 +0200

    Update dpc-130610-p1-verify-options-under-customer-account-button.js

--- cps-dashboard-plugin/tests/dpc-130610-p1-verify-options-under-customer-account-button.js
+++ cps-dashboard-plugin/tests/dpc-130610-p1-verify-options-under-customer-account-button.js
@@ -1,7 +1,3 @@
-/*
-    Author: Saurabh Patwardhan
- */
-
 describe("dpc-130610:Dashboard:P1:Verify customer account drop-down button options", function() {
     var frameworkUtils, loginUtils, dashboardUtils, edit_profileUtils;
     var framework_Utils = require("../../cp-utils/framework_utils.js");
