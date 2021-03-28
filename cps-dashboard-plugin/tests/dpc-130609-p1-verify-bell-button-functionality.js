d478d8ad5b616a0d024a021f448fe01caae883b5
firods <shrikantfirodiya4@gmail.com>
2021-03-28 12:31:16 +0200

    Update dpc-130609-p1-verify-bell-button-functionality.js

--- cps-dashboard-plugin/tests/dpc-130609-p1-verify-bell-button-functionality.js
+++ cps-dashboard-plugin/tests/dpc-130609-p1-verify-bell-button-functionality.js
@@ -1,7 +1,3 @@
-/*
-    Author: Saurabh Patwardhan
- */
-
 describe("dpc-130609:Dashboard:P1:Verify functionality of bell button", function() {
     var frameworkUtils, loginUtils, dashboardUtils, edit_profileUtils;
     var framework_Utils = require("../../cp-utils/framework_utils.js");
