3151c139d94532d51635337ba25b6c427b51e9ce
firods <shrikantfirodiya4@gmail.com>
2021-03-28 12:34:16 +0200

    Update dpc-130558-p1-notification-settings-remove-remove-all-recepients.js

--- cps-events-history-plugin/tests/dpc-130558-p1-notification-settings-remove-remove-all-recepients.js
+++ cps-events-history-plugin/tests/dpc-130558-p1-notification-settings-remove-remove-all-recepients.js
@@ -1,7 +1,3 @@
-/*
-    Author: Saurabh Patwardhan
- */
-
 describe("dpc-130558:EHN:P1:Notification Settings->Remove&RemoveAll recepients", function() {
     var frameworkUtils, loginUtils, dashboardUtils, edit_profileUtils;
     var framework_Utils = require("../../cp-utils/framework_utils.js");
