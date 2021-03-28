baf5b02f83f385430b94d98ef060e9b122b3c1fd
firods <shrikantfirodiya4@gmail.com>
2021-03-28 12:30:55 +0200

    Update dpc-130604-p1-verify-clicking-cloug-portal-logo-navigation.js

--- cps-dashboard-plugin/tests/dpc-130604-p1-verify-clicking-cloug-portal-logo-navigation.js
+++ cps-dashboard-plugin/tests/dpc-130604-p1-verify-clicking-cloug-portal-logo-navigation.js
@@ -1,7 +1,3 @@
-/*
-    Author: Saurabh Patwardhan
- */
-
 describe("dpc-130604:Dashboard:P1:Verify clicking on Cloud Portal logo navigates to dashboard", function() {
     var frameworkUtils, loginUtils, dashboardUtils, edit_profileUtils;
     var framework_Utils = require("../../cp-utils/framework_utils.js");
